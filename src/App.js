6038587339cbf79618067b5a1c81c72609757fad
The-Hawkeye <82801422+The-Hawkeye@users.noreply.github.com>
2023-12-23 16:58:35 +0530

    dsfad

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
     return () => {
       document.removeEventListener('click', handleOutsideClick);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [openForm]);
 
   return (
